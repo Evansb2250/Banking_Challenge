d90775956c7f6150319c86b0bbcc3b89e8d68ebc
samue <samuebrandenburg12@gmail.com>
2021-08-31 14:05:28 -0500

    adding libraries to project

---
+++ Veryable-coding-challange-A-/app/src/main/java/com/veryable/android/network/Webservice.kt
@@ -0,0 +1,9 @@
+package com.veryable.android.network
+
+/*
+1. create retrofit object
+2. create retrofit interface function
+3. define the interface function
+4. create an object of the Webservice
+
+ */
