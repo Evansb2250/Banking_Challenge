04a85586e639988e4bffa3324557df881691dc74
samue <samuebrandenburg12@gmail.com>
2021-09-02 01:01:50 -0500

    * added bindingAdapter to transform bank transfer info

--- Veryable-coding-challange-A-/app/src/main/java/com/veryable/android/bindingAdapters/BindingAdapters.kt
+++ Veryable-coding-challange-A-/app/src/main/java/com/veryable/android/bindingAdapters/BindingAdapters.kt
@@ -1,9 +1,12 @@
 package com.veryable.android.bindingAdapters
 
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.veryable.android.R
 import com.veryable.android.constants.BANK_STRING
+import com.veryable.android.constants.BANK_TRANSFER_SPEED
+import com.veryable.android.constants.CARD_TRANSFER_SPEED
 import com.veryable.android.domain.AccountDomain
 
 
@@ -15,4 +18,11 @@ fun bindAccountImage(imageView: ImageView, accountDomain: AccountDomain){
             else -> R.drawable.ic__763513_atm_card_bank_card_cash_card_credit_card_plastic_money_icon
         }
     )
+}
+
+
+
+@BindingAdapter("accountTransferSpeed")
+fun bindAccountTransferDetail(textView: TextView, accountDomain: AccountDomain){
+    textView.text = if(accountDomain.accountType.accountType.equals(BANK_STRING)) BANK_TRANSFER_SPEED else CARD_TRANSFER_SPEED
 }
--- Veryable-coding-challange-A-/app/src/main/java/com/veryable/android/constants/Constants.kt
+++ Veryable-coding-challange-A-/app/src/main/java/com/veryable/android/constants/Constants.kt
@@ -3,3 +3,5 @@ package com.veryable.android.constants
 
 const val CARD_STRING ="card"
 const val BANK_STRING ="bank"
+const val CARD_TRANSFER_SPEED = "Card: Instant"
+const val BANK_TRANSFER_SPEED = "Bank Account: ACH - Same Day"
